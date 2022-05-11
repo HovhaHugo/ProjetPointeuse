5b9b1d2bdc0eec7af14625e93fe1651df40c9fe1
Clement973 <105269269+Clement973@users.noreply.github.com>
2022-05-11 15:56:38 +0200

    Add files via upload

---
+++ src/StarkManagement/Score.java
@@ -0,0 +1,41 @@
+package StarkManagement;
+
+import Pointeuse.Hours;
+
+public class Score
+{
+    private Employee employee;
+    private Hours heure;
+    private Historique Historique;
+
+
+    public Score(Employee employee, Hours heure, StarkManagement.Historique historique) {
+        this.employee = employee;
+        this.heure = heure;
+        Historique = historique;
+    }
+
+    public Hours getHeure() {
+        return heure;
+    }
+
+    public void setHeure(Hours heure) {
+        this.heure = heure;
+    }
+
+    public StarkManagement.Historique getHistorique() {
+        return Historique;
+    }
+
+    public void setHistorique(StarkManagement.Historique historique) {
+        Historique = historique;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+}
