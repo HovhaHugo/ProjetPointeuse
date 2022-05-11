60879fb7ebec996515ef96df0b04cfca8208e53b
AimadAbdelouhab <a.aimad1999@gmail.com>
2022-05-11 15:39:52 +0200

    feat : Ajout de la classe Employee et Department

---
+++ src/StarkManagement/Model/Department.java
@@ -0,0 +1,34 @@
+package StarkManagement.Model;
+
+import java.util.ArrayList;
+
+public class Department {
+    private String nameDepartment;
+    private ArrayList<Employee> listEmployee ;
+
+
+    public Department() {
+        this.listEmployee = new ArrayList<Employee>();
+    }
+
+    public Department(String nameDepartment) {
+        this.nameDepartment = nameDepartment;
+        this.listEmployee = new ArrayList<Employee>();
+    }
+
+    public String getNameDepartment() {
+        return nameDepartment;
+    }
+
+    public void setNameDepartment(String nameDepartment) {
+        this.nameDepartment = nameDepartment;
+    }
+
+    public ArrayList<Employee> getListEmployee() {
+        return listEmployee;
+    }
+
+    public void setListEmployee(ArrayList<Employee> listEmployee) {
+        this.listEmployee = listEmployee;
+    }
+}
