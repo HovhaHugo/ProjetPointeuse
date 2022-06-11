28bc0b2305ef16541e436a34b69e35210e822e44
Hugo HOVHANNESSIAN <hugo.hovhannessian@etu.univ-tours.fr>
2022-06-11 16:09:32 +0200

    update : Modifiaction de la classe SettingControler (suppression des fonctions inutiles)

--- src/StarkManagement/Controller/SettingControler.java
+++ src/StarkManagement/Controller/SettingControler.java
@@ -3,21 +3,8 @@ package StarkManagement.Controller;
 import StarkManagement.Model.FileManipulator;
 import StarkManagement.Model.Settings;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-
 public class SettingControler {
-    static Settings server = new Settings("localhost", 8000);
     static Settings pointeuse = new Settings("localhost", 8000);
-
-    public static String getPointeuseAdress(){
-        return pointeuse.getIp();
-    }
-
-    public static int getPointeusePort(){
-        return pointeuse.getPort();
-    }
 
     public static void update(int newPort, String newAdress){
         pointeuse.setPort(newPort);
@@ -28,7 +15,4 @@ public class SettingControler {
         FileManipulator.exportMainAppSetting(pointeuse);
     }
 
-    public static void importeSetting(){
-        pointeuse = FileManipulator.importMainAppSetting();
-    }
 }
